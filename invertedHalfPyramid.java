eff0e6377fe406c1218d05f15287af6b8083d1de
Divyaa Sharmaa <shdivya1310@gmail.com>
2023-09-04 00:00:59 +0530

    Inverted Half-Pyramid

--- invertedHalfPyramid.java
+++ invertedHalfPyramid.java
@@ -7,7 +7,7 @@ public class invertedHalfPyramid {
     public static void main(String[] args){
         int noOfLines = sc.nextInt();
         for(int i = 1;i<=noOfLines;i++){
-            for(int j = 1; j<=5-i+1;j++){
+            for(int j = 1; j<=noOfLines-i+1;j++){
                 System.out.print(j);
             }
             System.out.println();
