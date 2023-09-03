6e5a405a931cc3d95ae627653a3bdaf041d36d52
Divyaa Sharmaa <shdivya1310@gmail.com>
2023-09-03 22:57:54 +0530

    Half-Pyramid Pattern

---
+++ charPattern.java
@@ -0,0 +1,18 @@
+package Patterns_in_java;
+
+import java.util.Scanner;
+
+public class charPattern {
+    public static Scanner sc = new Scanner(System.in); 
+    public static void main(String[] args){
+        int noOfLines = sc.nextInt();
+        char ch = 'A';
+        for(int i = 1;i<=noOfLines;i++){
+            for(int j = 1; j<=i;j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
