6b58dfff97a4ff358f1d93d2b8b8ac3c7b5e6ba8
Divyaa Sharmaa <shdivya1310@gmail.com>
2023-09-03 22:40:53 +0530

    Star Patterns

---
+++ squarePattern.java
@@ -0,0 +1,16 @@
+package Patterns_in_java;
+
+import java.util.Scanner;
+
+public class squarePattern {
+    public static Scanner sc = new Scanner(System.in); 
+    public static void main(String[] args){
+        int noOfLines = sc.nextInt();
+        for(int i = 1;i<=noOfLines;i++){
+            for(int j = 1; j<=noOfLines;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    } 
+}
