8dced6b90f0df21a8e5730d36d7ca3b031640986
Divyaa Sharmaa <shdivya1310@gmail.com>
2023-09-03 22:59:55 +0530

    Character Pattern

--- charPattern.java
+++ charPattern.java
@@ -6,10 +6,10 @@ public class charPattern {
     public static Scanner sc = new Scanner(System.in); 
     public static void main(String[] args){
         int noOfLines = sc.nextInt();
-        char ch = 'A';
+        char ch = 'A';  
         for(int i = 1;i<=noOfLines;i++){
             for(int j = 1; j<=i;j++){
-                System.out.print(ch);
+                System.out.print(ch);   
                 ch++;
             }
             System.out.println();
